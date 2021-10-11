cdf7cb4953f8065f62521a5727ded063c6ef611b
Ewa <ewa.rosset@softwire.com>
2021-10-11 14:03:32 +0100

    Added bishop's available moves

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -10,7 +11,120 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+            List<Square> moves = new List<Square>();
+            
+            // rightup row-1 col+1
+            
+            for (int col = currentSquare.Col + 1; col <= 7; col++)
+            {
+                for (int row = currentSquare.Row - 1; row >= 0; row--)
+                {
+                    Console.WriteLine("row: " + row + "col: " + col);
+
+                    if (!board.IsWithinBoardInt(row, col))
+                    {
+                        continue;
+                    }
+
+                    if (currentSquare == new Square(row, col))
+                    {
+                        continue;
+                    }
+
+                    var nextSquare = new Square(row, col);
+                    var nextPiece = board.GetPiece(nextSquare);
+
+                    if (nextPiece == null || nextPiece.Player != Player)
+                    {
+                        moves.Add(nextSquare);
+                    }
+                }
+            }
+            
+            // rightdown row+1 col +1
+            
+            for (int col = currentSquare.Col + 1; col <= 7; col++)
+            {
+                for (int row = currentSquare.Row +1; row <= 7; row++)
+                {
+                    Console.WriteLine("row: " + row + "col: " + col);
+
+                    if (!board.IsWithinBoardInt(row, col))
+                    {
+                        continue;
+                    }
+
+                    if (currentSquare == new Square(row, col))
+                    {
+                        continue;
+                    }
+
+                    var nextSquare = new Square(row, col);
+                    var nextPiece = board.GetPiece(nextSquare);
+
+                    if (nextPiece == null || nextPiece.Player != Player)
+                    {
+                        moves.Add(nextSquare);
+                    }
+                }
+            }
+            // leftup row-1, col-1
+            
+            for (int col = currentSquare.Col - 1; col >= 0; col--)
+            {
+                for (int row = currentSquare.Row - 1; row >= 0; row--)
+                {
+                    Console.WriteLine("row: " + row + "col: " + col);
+
+                    if (!board.IsWithinBoardInt(row, col))
+                    {
+                        continue;
+                    }
+
+                    if (currentSquare == new Square(row, col))
+                    {
+                        continue;
+                    }
+
+                    var nextSquare = new Square(row, col);
+                    var nextPiece = board.GetPiece(nextSquare);
+
+                    if (nextPiece == null || nextPiece.Player != Player)
+                    {
+                        moves.Add(nextSquare);
+                    }
+                }
+            }
+            // leftdown row+1, col-1
+            
+            for (int col = currentSquare.Col - 1; col >= 0; col--)
+            {
+                for (int row = currentSquare.Row +1; row <= 7; row++)
+                {
+                    Console.WriteLine("row: " + row + "col: " + col);
+
+                    if (!board.IsWithinBoardInt(row, col))
+                    {
+                        continue;
+                    }
+
+                    if (currentSquare == new Square(row, col))
+                    {
+                        continue;
+                    }
+
+                    var nextSquare = new Square(row, col);
+                    var nextPiece = board.GetPiece(nextSquare);
+
+                    if (nextPiece == null || nextPiece.Player != Player)
+                    {
+                        moves.Add(nextSquare);
+                    }
+                }
+            }
+
+            return moves;
         }
     }
 }
