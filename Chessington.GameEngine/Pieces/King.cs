e0f7cb99031b8129ddbaf9806ab6d303001379bb
Ewa <ewa.rosset@softwire.com>
2021-10-11 09:12:35 +0100

    Create moving functionality for the king

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -1,16 +1,52 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 
 namespace Chessington.GameEngine.Pieces
 {
     public class King : Piece
     {
         public King(Player player)
-            : base(player) { }
+            : base(player)
+        {
+        }
+
+        
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+            List<Square> moves = new List<Square>();
+            
+            for (int col = currentSquare.Col - 1; col <= currentSquare.Row + 1; col++)
+            {
+                for (int row = currentSquare.Row - 1; row <= currentSquare.Row + 1; row++)
+                {
+                    Console.WriteLine("row: " + row + "col: " + col);
+
+                    if (!board.IsWithinBoard(row, col))
+                    {
+                        continue;
+                    }
+
+                    if (currentSquare == new Square(row, col))
+                    {
+                        continue;
+                    }
+
+                    var nextSquare = new Square(row, col);
+                    var nextPiece = board.GetPiece(nextSquare);
+
+                    if (nextPiece == null || nextPiece.Player != Player)
+                    {
+                        moves.Add(nextSquare);
+                    }
+                }
+            }
+            
+            return moves;
+
         }
     }
 }
