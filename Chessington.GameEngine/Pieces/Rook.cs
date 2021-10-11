b2a7f146dccbfb4bb9fdb12f220bdd44d39173b3
Ewa <ewa.rosset@softwire.com>
2021-10-11 09:11:49 +0100

    Add moving functionality fro the rook

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,111 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+            List<Square> moves = new List<Square>();
+
+            int direction;
+            int right;
+            int left;
+            
+            if (Player == Player.White)
+            {
+                direction = -1;
+                right = 1;
+                left = -1;
+            }
+            else
+            {
+                direction = 1;
+                right = -1;
+                left = 1;
+            }
+            
+            
+            // moving right
+            for (int index = currentSquare.Col + 1; index <= 7; index++)
+            {
+                var nextSquare = new Square(currentSquare.Row, index);
+                var rightPiece = board.GetPiece(nextSquare);
+
+                if (rightPiece != null)
+                {
+                    if (rightPiece.Player != Player)
+                    {
+                        moves.Add(nextSquare);
+                    }
+
+                    break;
+                }
+                
+                moves.Add(nextSquare);
+            }
+
+            //moving left
+            
+            for (int index = currentSquare.Col - 1; index >= 0; index--)
+            {
+                var nextSquare = new Square(currentSquare.Row, index);
+                var leftPiece = board.GetPiece(nextSquare);
+
+                if (leftPiece != null)
+                {
+                    if (leftPiece.Player != Player)
+                    {
+                        moves.Add(nextSquare);
+                    }
+
+                    break;
+                }
+                
+                moves.Add(nextSquare);
+            }
+            
+            //moving up
+            
+            for (int index = currentSquare.Row - 1; index >= 0; index--)
+            {
+                var nextSquare = new Square(index, currentSquare.Col);
+                var upPiece = board.GetPiece(nextSquare);
+
+                if (upPiece != null)
+                {
+                    if (upPiece.Player != Player)
+                    {
+                        moves.Add(nextSquare);
+                    }
+
+                    break;
+                }
+                
+                moves.Add(nextSquare);
+            }
+            
+            //moving down
+            
+            for (int index = currentSquare.Row + 1; index <= 7; index++)
+            {
+                var nextSquare = new Square(index, currentSquare.Col);
+                var downPiece = board.GetPiece(nextSquare);
+
+                if (downPiece != null)
+                {
+                    if (downPiece.Player != Player)
+                    {
+                        moves.Add(nextSquare);
+                    }
+
+                    break;
+                }
+                
+                moves.Add(nextSquare);
+            }
+            
+            return moves;
+            
+            
+            
+
         }
     }
 }
